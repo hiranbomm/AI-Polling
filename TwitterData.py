68bb25d6b030c565c49fe41410631b9240b02418
Hiran <hiran.bomm@gmail.com>
2019-07-13 23:31:53 -0500

    started polarity calculations

--- TwitterData.py
+++ TwitterData.py
@@ -1,37 +1,46 @@
 import tweepy
-import csv, re, sys
-import pandas as pd
-from textblob import TextBlob
+import re
+
+
 
 class TwitterData(object):
+
 
     def __init__(self, candidates):
         self.all_tweets = []
         self.API = None
+        self.polarity = None
+        self.positive = None
+        self.negative = None
+
         # self.candidates = candidates
+
 
     def authorize(self):
         try:
             # keys deleted for privacy
+            consumerKey = 'iv5nToJwQNSA3xVeskWP5on5G'
+            consumerSecret = 'P93uV3esVxMu5mh2nNHJDj3iTUr5SRzfMGE1Vdg8GGsK1VOOkA'
+
+            accessKey = '1150145735259807745-m0IxHzaMRFWNV3GndR82Kwu80IuywP'
+            accessSecret = 'tTzYHIsbbZMnD6zCwa4QR46GjDHTh91QUP1zXmwXztEYu'
 
             authorized = tweepy.OAuthHandler(consumerKey, consumerSecret)
             authorized.set_access_token(accessKey, accessSecret)
 
-            self.API = tweepy.API(authorized)
+            self.API = tweepy.API(authorized) # , wait_on_rate_limit=True)
 
         except:
             print("ERROR: AUTHENTICATION FOR API FAILED")
 
 
-
-    def getData(self, candidate):
-        print("getting data")
+    def getData(self, candidate, NUM_TWEETS):
+        print("getting tweets")
         self.all_tweets.clear()
 
-        for tweet in tweepy.Cursor(self.API.search, q=candidate, lang='en').items(100):
-            self.all_tweets.append(tweet)
+        # for each tweet returned, get rid of unwanted symbols and add it to all_tweets
+        for tweet in tweepy.Cursor(self.API.search, q=candidate, lang='en').items(NUM_TWEETS):
+            text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|(RT)", " ", tweet.text).split())
+            self.all_tweets.append(text) # .encode('utf-8'))
 
         print("done getting data")
-
-
-
--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-import tweepy
 import csv, re, sys
 import pandas as pd
 from textblob import TextBlob
 from TwitterData import TwitterData
 
+# TODO: cache the data into a file and read it from there. If file doesnt exist, then call the API for fresh data
+# TODO:     OR: set a flag for newData. if it is true, then rewrite the file. else, use the existing file
+
+NUM_TWEETS = 100
+POS_POL = 0.2   # "I love ___ " got polarity of 0.5, so aiming a bit lower to consider a tweet positive
+NEG_POL = -0.5  # "I hate ___ " got polarity of -0.8, so aiming a bit higher to consider a tweet negative
 
 def init():
-    candidates = ['Bernie Sanders']
+    candidates = ['Bernie Sanders', 'Donald Trump']
+    pol_dict = {}
 
     TD = TwitterData(candidates)
     TD.authorize()
     for candidate in candidates:
-        TD.getData(candidate)
+        print("current candidate: ", candidate)
+        TD.getData(candidate, NUM_TWEETS)
 
-    print(TD.all_tweets)
+        print("calculating polarity")
+        TD.negative = 0
+        TD.positive = 0
+
+        for tweet in TD.all_tweets:
+            pol = getPolarity(tweet)
+            if pol < 0: TD.negative += 1
+            elif pol > 0: TD.positive += 1
+
+        pol_dict[candidate] = (TD.positive, TD.negative, len(TD.all_tweets)) # TODO: make sure this tuple goes into the dictionary well
+
+    print(pol_dict)
+
+
+# returns -1 if polarity is negative, 1 if positive and 0 otherwise
+def getPolarity(tweet):
+
+    blob = TextBlob(tweet)
+    if blob.sentiment.polarity >= POS_POL:
+        return 1
+    elif blob.sentiment.polarity <= NEG_POL:
+        return -1
+    else:
+        return 0
+
+
 
 if __name__ == "__main__":
     init()
